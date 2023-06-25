d3be7e625e8b1545df9bbb5a93fd87c3d61a769c
The-Great-Toad <bearcode26@gmail.com>
2023-06-25 19:39:54 +0200

    affichage de donnée + réagir aux évènements

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
-  styleUrls: ['./face-snap.component.scss']
+  styleUrls: ['./face-snap.component.scss'],
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  hasSnapped: boolean = false;
 
+  ngOnInit(): void {
+    this.title = 'Naruto';
+    this.description = 'You better believe it!';
+    this.createdDate = new Date();
+    this.snaps = 8;
+    this.imageUrl =
+      'https://static.vecteezy.com/ti/vecteur-libre/p3/14845288-illustration-de-personnage-naruto-iconele-style-cartoon-plat-gratuit-vectoriel.jpg';
+  }
+
+  onSnap() {
+    if (!this.hasSnapped) {
+      this.hasSnapped = true;
+      this.snaps++;
+    } else {
+      this.snaps--;
+      this.hasSnapped = false;
+    }
+  }
 }
