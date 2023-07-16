4b5840deb99c01cfc5f97fed7fe068923d39f8e9
The-Great-Toad <bearcode26@gmail.com>
2023-07-16 14:37:58 +0200

    éviter les fuites de mémoires

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snape.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { Subject, interval, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss'],
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private faceSnapsService: FaceSnapsService) {}
 
   ngOnInit() {
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+    this.destroy$ = new Subject<boolean>();
+
+    interval(1000)
+      .pipe(
+        // take(3),
+        tap(console.log),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 }
